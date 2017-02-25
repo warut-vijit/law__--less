5aa2c1b6917c44cf95edf926f2caa7f6d983f6fb
nriaziat <nriaziat@gmail.com>
2017-02-25 17:03:44 -0500

    added comments

--- xmlCleaner.py
+++ xmlCleaner.py
@@ -1,17 +1,29 @@
 import os
 
+# function removes ampersand and replaces misformated
+
 def cleaner(filename):
+
+    # output file name appends a 'b'
+
     output = filename[:-4] + 'b' + '.xml'
     with open(filename) as xml:
         with open(output, 'a+') as output:
             for line in xml:
+
+                # iterates line by line through the xml and removes ampersands/errors
+
                 line = line.replace('\"id=', 'id=\"')
                 line = line.replace('&', '')
                 output.write(line)
 
 
-for filename in os.listdir("C:/Users/naveed/Documents/fulltext"):
-    if filename == 'textScript.py':
+fileDir = input("Input path to files to be cleaned: ")
+for filename in os.listdir(fileDir):
+
+    # ignores its own file
+
+    if filename == 'xmlCleaner.py':
         pass
     else:
         cleaner(filename)
