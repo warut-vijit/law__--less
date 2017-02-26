5a6e0478cdb56d7b07d4e88f7c82380676cfe21f
NAN1248 <abnanc@gmail.com>
2017-02-25 18:03:31 -0600

    bs4 prototype working

--- xmlParser.py
+++ xmlParser.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
-
-soup = BeautifulSoup("clean.xml", 'xml')
-sentences = soup.find_all('sentece')
-for sentence in senteces:
-    print sentece.text
+with open("06_1b.xml") as inputFile:
+    soup = BeautifulSoup(inputFile, 'xml')
+    sentences = soup.find_all('sentence')
+    for sentence in sentences:
+        print sentence.text
