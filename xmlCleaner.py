b1db588521b90f4f8922f1b40d2e0f5cbf7e9e5f
NAN1248 <abnanc@gmail.com>
2017-02-25 18:38:56 -0600

    unified cleaner and parcer

--- xmlCleaner.py
+++ xmlCleaner.py
@@ -1,11 +1,15 @@
 import os
-# function removes ampersand and replaces misformated
+from bs4 import BeautifulSoup
+
+# function removes ampersand and replaces misformated data.
+# outputs end as xml
 
 def cleaner(filename):
 
     # output file name appends a 'b'
 
     output = filename[:-4] + 'b' + '.xml'
+
     with open(filename) as xml:
         with open(output, 'w+') as output:
             for line in xml:
@@ -15,14 +19,36 @@ def cleaner(filename):
                 line = line.replace('\"id=', 'id=\"')
                 line = line.replace('&', '')
                 line = line.replace('\n', '')
+                line = line.replace('[', '')
+                line = line.replace(']', '')
+                line = line.replace('\'', '')
+                line = line.replace('\"', '')
                 output.write(line)
+
+def xmlToText(filename):
+
+    # convert xml input to .txt output
+
+    with open(filename) as inputFile:
+        with open(filename[:-4] + '.txt', 'w+') as output:
+            soup = BeautifulSoup(inputFile, 'xml')
+            sentences = soup.find_all('sentence')
+            for sentence in sentences:
+                output.write(sentence.text)
 
 
 for filename in os.listdir(os.getcwd()): # searches through all files in working directory
 
-    # ignores its own file name
+    # only uses .xml file
 
-    if filename == 'xmlCleaner.py':
+    if filename[-4:] != '.xml':
         pass
     else:
         cleaner(filename)
+        xmlToText(filename)
+
+for filename in os.listdir(os.getcwd()): # searches through all files in working directory
+
+    if filename[-4:] == '.xml':
+        print('deleting')
+        os.remove(filename)
--- xmlParser.py
+++ xmlParser.py
@@ -1,6 +1,17 @@
 from bs4 import BeautifulSoup
-with open("06_1b.xml") as inputFile:
-    soup = BeautifulSoup(inputFile, 'xml')
-    sentences = soup.find_all('sentence')
-    for sentence in sentences:
-        print sentence.text
+import os
+
+def textMaker(filename):
+    with open(filename) as inputFile:
+        with open(filename[:-4] + '.txt', 'w+') as output:
+            soup = BeautifulSoup(inputFile, 'xml')
+            sentences = soup.find_all('sentence')
+            for sentence in sentences:
+                output.write(sentence.text)
+
+
+for filename in os.listdir(os.getcwd()):
+    if filename[-4:] != '.xml':
+        pass
+    else:
+        textMaker(filename)
